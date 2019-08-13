8437995962c49923a421eb94380619542eae3ce2
Lauren Dove <elledoveinc@gmail.com>
2019-08-13 03:28:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
-end
+def square_array(array) #(1,2,3)
+  new_numbers = []
+counter =0
+  while counter < array.length do #3
+new_numbers<< array[counter]**2  
+    counter +=1
+  end
+  new_numbers 
+ end 
+ 
